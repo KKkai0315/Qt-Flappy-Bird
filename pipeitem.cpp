c3fcdccd09eb28bc37926c22bfc3b8c513d4d07f
KKkai <1640576073@qq.com>
2023-04-23 15:38:09 +0800

    pipe position fixed

--- pipeitem.cpp
+++ pipeitem.cpp
@@ -1,7 +1,9 @@
 #include "pipeitem.h"
+#include <QPointF>
 
 PipeItem::PipeItem() :
     apipe(new QGraphicsPixmapItem(QPixmap(":/new/prefix1/pipe.png")))
 {
+    apipe->setPos(QPointF(0,0) - QPointF(apipe->boundingRect().width(),apipe->boundingRect().height()-260));
     addToGroup(apipe);
 }
